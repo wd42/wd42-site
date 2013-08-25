748f02627377d1e2e5c244dbcf9a76758c326abf
Jonny Scholes <jonnyscholes@gmail.com>
2013-08-25 12:55:28 +1000

    Lanyard integration

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
+lanyard = require('lanyard-speakers');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Welcome to Web Developer 42&deg;' });
+  var options = {
+    hostname: 'lanyrd.com',
+    path: '/series/wd42/'
+  }
+
+  lanyard.getSpeakers(options, function(speakers){
+    res.render('index', { title: 'Welcome to Web Developer 42&deg;', speakers: speakers });
+  });
 };
