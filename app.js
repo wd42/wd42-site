d216c3a1e928d0fa503f3f2547cb661ec431bb66
joshgillies <github@joshgilli.es>
2013-08-28 01:26:56 +1000

    Tidied up download script, no more weird passing of setTimeout timeouts.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path')
-  , download = require('./lib/download');
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var download = require('./lib/download')();
 
 var app = express();
-
-download();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
