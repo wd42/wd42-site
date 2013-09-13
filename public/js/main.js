7074ff53055a41625b8f02c9c2044da0f9447f7a
Jonny Scholes <jonnyscholes@gmail.com>
2013-09-13 11:29:13 +1000

    Added some images [not yet in content], reintegrated lanyard-speakers [make sure to update to latest V], starts reworking mobile

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+$(window).resize(function(){
+  var imgHeight = $('.featureImage').height();
+  $('nav a').each(function(){
+    var p = $(this).css('bottom');
+    console.log(p);
+  });
+});
+
+
+var backgroundAccordian = function(){
+
+  var today = '';
+
+}
